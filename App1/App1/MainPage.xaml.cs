7a247cea8723248e5ff32af707e3fd8acdb8bf6b
Jakub Vančata <vancata.kuba@seznam.cz>
2017-11-21 09:20:43 +0100

    Init

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -9,9 +9,33 @@ namespace App1
 {
     public partial class MainPage : ContentPage
     {
+        private bool Matched = false;
         public MainPage()
         {
             InitializeComponent();
+        }
+        public void checkPalindrome(string input)
+        {
+            int a = input.Length-1;
+            for (int i = 0; i < a-1; i++)
+            {
+                if(input[i] == input[a])
+                {
+                    Matched = true;
+                }
+                else
+                {
+                    Matched = false;
+                }
+                
+                Vypis.Text = Matched ? "Tohle je palindrom, bro" : "Tohle neni palindrom, bro";
+                a--;
+            }
+        }
+        void Okay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var oldText = e.OldTextValue;
+            var newText = e.NewTextValue;
         }
     }
 }
